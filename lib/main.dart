5a8bace2bdc04ea2df68717bbb64ea1a034a5bad
brandon castellanos <bcas252@wgu.edu>
2025-09-12 16:36:02 -0500

    Added saving/loading functionality and deleting completed tasks

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'dart:convert'; //for jsonEncoded/jsonDecode
+import 'package:shared_preferences/shared_preferences.dart';
 void main() {
   runApp(const TodoApp());
 }
@@ -22,7 +23,21 @@ class Task{
   String title;
   bool isDone;
   Task({required this.title, this.isDone = false});
+
+  //convert Task -> Map (for JSON)
+  Map<String, dynamic> toMap(){
+    return {'title': title, 'isDone': isDone};
+  }
+
+  //convert Map -> Task
+  factory Task.fromMap(Map<String, dynamic> map){
+    return Task(
+      title: map['title'],
+      isDone : map['isDone'],
+    );
+  }
 }
+
 
 class TodoPage extends StatefulWidget{
   const TodoPage({super.key});
@@ -35,6 +50,34 @@ class _TodoPageState extends State<TodoPage> {
   final List<Task> _todos = []; // our task list
   final TextEditingController _controller = TextEditingController();
 
+  @override
+  void initState() {
+    super.initState();
+    _loadTodos(); // load tasks on startup
+  }
+
+  // save tasks locally
+  Future<void> _saveTodos() async {
+    final prefs = await SharedPreferences.getInstance();
+    final List<String> taskList =
+        _todos.map((task) => jsonEncode(task.toMap())).toList();
+    await prefs.setStringList('todos', taskList);
+  }
+
+  // load tasks locally
+  Future<void> _loadTodos() async {
+    final prefs = await SharedPreferences.getInstance();
+    final List<String>? taskList = prefs.getStringList('todos');
+
+    if(taskList != null){
+      setState(() {
+        _todos.clear();
+        _todos.addAll(
+          taskList.map((item) => Task.fromMap(jsonDecode(item))),
+        );
+      });
+    }
+  }
   //add a task
   void _addTodo(){
     if (_controller.text.trim().isEmpty) return;
@@ -42,6 +85,7 @@ class _TodoPageState extends State<TodoPage> {
       _todos.add(Task(title: _controller.text.trim()));
       _controller.clear();
     });
+    _saveTodos();
   }
 
   //remove a task
@@ -49,6 +93,7 @@ class _TodoPageState extends State<TodoPage> {
     setState(() {
       _todos.removeAt(index);
     });
+    _saveTodos();
   }
 
   // toggle "done" state
@@ -56,14 +101,33 @@ class _TodoPageState extends State<TodoPage> {
     setState(() {
       _todos[index].isDone = !_todos[index].isDone;
     });
+    _saveTodos();
+  }
+
+  // Clear completed tasks
+  void _clearCompleted() {
+    setState(() {
+      _todos.removeWhere((task) => task.isDone);
+    });
+    _saveTodos();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Task App")),
+      appBar: AppBar(
+          title: const Text("My To-Do List"),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.delete_sweep),
+              tooltip: "Clear Completed",
+              onPressed: _clearCompleted,
+            ),
+          ],
+      ),
       body: Column(
         children: [
+          // input row
           Padding(
             padding: const EdgeInsets.all(12.0),
             child: Row(
@@ -77,15 +141,15 @@ class _TodoPageState extends State<TodoPage> {
                     ),
                   ),
                 ),
-              const SizedBox(width: 8),
-              ElevatedButton(
-                onPressed: _addTodo,
-                child: const Text("Add"),
-              )
+                const SizedBox(width: 8),
+                ElevatedButton(
+                  onPressed: _addTodo,
+                  child: const Text("Add"),
+                )
               ],
             ),
           ),
-
+          // task list
           Expanded(
             child: ListView.builder(
               itemCount: _todos.length,
